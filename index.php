e47c984ca480506d353b6b0dccb6bf5bb0fb72fe
Eduardo Ataide <eataide@outlook.com>
2026-09-10 23:45:36 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+$arr = array('um', 'dois', 'três', 'quatro', 'pare', 'cinco');
+foreach ($arr as $val) {
+    if ($val == 'três') {
+        break;    /* Pode-se também usar 'break 1;' aqui. */
+    }
+    echo "$val\n";
+}
