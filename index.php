065d88c48676abfd49ce4e9636fc9a6a0e9655b3
Eduardo Ataide <eataide.ti@outlook.com>
2026-09-12 10:01:53 -0300

    Alteração de código

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $teste1 = 123;
 
 $arr = array('um', 'dois', 'três', 'quatro', 'pare', 'cinco');
 foreach ($arr as $val) {
-    if ($val == 'três') {
+    if ($val == 'pare') {
         break;    /* Pode-se também usar 'break 1;' aqui. */
     }
     echo "$val\n";
